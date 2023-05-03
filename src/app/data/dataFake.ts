fb066538d5fde20f9724b61896c450f7deecba85
felipebrenno0 <felipebrenno213@gmail.com>
2023-05-03 10:13:02 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Saiu a nova versão do Angular",
+        "description": "",
+        "photoCover": "https://devporai.com.br/wp-content/uploads/2020/06/Copy-of-Copy-of-Travel-Photography.png"
+    },
+
+    {
+        "id": "2",
+        "title": "React Native vs. Angular for Mobile Apps",
+        "description": "ola",
+        "photoCover": "https://fullscale.io/wp-content/uploads/2022/04/react-native-and-angular-in-mobile-apps.png"
+    },
+    {
+        "id": "3",
+        "title": "Best Way To Build Web And Mobile Apps With Angular And NativeScript",
+        "description": "ola",
+        "photoCover": "https://www.appstory.org/wp-content/uploads/2019/05/how-it-works-min.png"
+    },
+    {
+        "id": "4",
+        "title": "Angular.JS Hybrid Mobile App Framework For Cross-Platform Mobile App Development",
+        "description": "ola",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy99sD2S2ymWewA9jq4-gynlcsM-Wv9pJ_9g&usqp=CAU"
+    },
+    {
+        "id": "5",
+        "title": "Building Native Mobile Apps Using Native Script in Angular 6 - Helios Blog",
+        "description": "teste",
+        "photoCover": "https://www.heliossolutions.co/blog/wp-content/uploads/2018/09/AngularJS-NativeScript-Development-company-1280x720.jpg"
+    }
+
+]
